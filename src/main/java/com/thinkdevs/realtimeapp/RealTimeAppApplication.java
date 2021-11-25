22fd772de58e76737e9760740a594531d1de63ab
kevin janvier <kevin.janvier5@gmail.com>
2021-11-25 15:32:12 +0300

    feat : create sample project

---
+++ src/main/java/com/thinkdevs/realtimeapp/RealTimeAppApplication.java
@@ -0,0 +1,13 @@
+package com.thinkdevs.realtimeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thinkdevs/realtimeapp/RealTimeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkdevs.realtimeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
